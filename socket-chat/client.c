f2eb6b6a9ffbafd2adef7414e1d7ab57a245d1e5
thuanle712 <lethuan712ngoc@gmail.com>
2023-11-24 17:19:29 +0700

    feat: updating component to show new message in code

--- socket-chat/client.c
+++ socket-chat/client.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char const *argv[])
 {
-    int sockfd = -1; // create socket 
-    struct sockaddr_in server_addr;    // chua dia chi cua server
-    char send_buffer[1024]; // send data client <---> server
+    int sockfd = -1;
+    struct sockaddr_in server_addr;
+    char send_buffer[1024];
     char recv_buffer[1024];
     time_t ticks;
 
@@ -21,42 +21,39 @@ int main(int argc, char const *argv[])
     memset(&server_addr, 0, sizeof(server_addr));
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         perror("socket");
         return 1;
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // inet_addr: convert chuoi string ip server theo kieu big endian
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(5000);
 
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("connect");
         close(sockfd);
         return 1;
     }
 
-    // doc message tu server
     read(sockfd, recv_buffer, sizeof(recv_buffer) - 1);
     printf("%s", recv_buffer);
 
-    while (1) {
-        // nhan dau vao
+    while (1)
+    {
         printf("Client: ");
         fgets(send_buffer, sizeof(send_buffer), stdin);
 
-        // gui message den server
         write(sockfd, send_buffer, strlen(send_buffer));
-
-        // kiem tra client co muon thoat khong "bye"
-        if (strcmp(send_buffer, "bye\n") == 0) {
-            // Server's goodbye message
+        if (strcmp(send_buffer, "quit\n") == 0)
+        {
             read(sockfd, recv_buffer, sizeof(recv_buffer) - 1);
             printf("%s", recv_buffer);
             break;
         }
 
-        // nhan va hien thi, tra loi den server
         memset(recv_buffer, 0, sizeof(recv_buffer));
         read(sockfd, recv_buffer, sizeof(recv_buffer) - 1);
         printf("Server: %s", recv_buffer);
--- socket-chat/server.c
+++ socket-chat/server.c
@@ -8,13 +8,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
     int listenfd = -1;
     int connfd = -1;
-    struct sockaddr_in server_addr; // chua dia chi cua server
-    struct sockaddr_in client_addr; // chua dia chi cua client
-    socklen_t client_len; // luu tru kich thuoc dia chi client
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_len;
     char send_buffer[1024];
     char recv_buffer[1024];
     time_t ticks;
@@ -24,7 +24,8 @@ int main(int argc, char const* argv[])
     memset(&server_addr, 0, sizeof(server_addr));
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd == -1) {
+    if (listenfd == -1)
+    {
         perror("socket");
         return 1;
     }
@@ -33,7 +34,8 @@ int main(int argc, char const* argv[])
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(5000);
 
-    if (bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("bind");
         close(listenfd);
         return 1;
@@ -45,37 +47,35 @@ int main(int argc, char const* argv[])
 
     while (1)
     {
-        client_len = sizeof(client_addr); // khoi tao kich thuoc dia chi client
-        connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_len);
-        if (connfd == -1) {
+        client_len = sizeof(client_addr);
+        connfd = accept(listenfd, (struct sockaddr *)&client_addr, &client_len);
+        if (connfd == -1)
+        {
             perror("accept");
-            continue; 
+            continue;
         }
 
         ticks = time(NULL);
         sprintf(send_buffer, "Server: Connected. Type 'bye' to quit.\n");
         write(connfd, send_buffer, strlen(send_buffer));
 
-        while (1) {
-            // nhan message tu client 
+        while (1)
+        {
             memset(recv_buffer, 0, sizeof(recv_buffer));
             read(connfd, recv_buffer, sizeof(recv_buffer) - 1);
 
-            if (strcmp(recv_buffer, "bye\n") == 0) {
-                // Client wants to quit, close the connection
+            if (strcmp(recv_buffer, "bye\n") == 0)
+            {
                 sprintf(send_buffer, "Server: Goodbye!\n");
                 write(connfd, send_buffer, strlen(send_buffer));
                 close(connfd);
-                break; 
+                break;
             }
 
             printf("Client: %s", recv_buffer);
-
-            // nhan input user send & rep for client
             printf("Server: ");
-            fgets(send_buffer, sizeof(send_buffer), stdin);
 
-            // gui<->tra loi cho client
+            fgets(send_buffer, sizeof(send_buffer), stdin);
             write(connfd, send_buffer, strlen(send_buffer));
         }
     }
