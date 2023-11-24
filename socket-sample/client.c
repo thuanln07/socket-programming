7e35911bddd8978792d4ea5b6b2235df7e622001
thuanle712 <lethuan712ngoc@gmail.com>
2023-11-24 17:20:57 +0700

    feat: updated socket sample

--- socket-sample/client.c
+++ socket-sample/client.c
@@ -9,29 +9,25 @@
 #include <unistd.h>
 #include <stdio.h>
 
-
 int main(int argc, char const *argv[])
 {
-	int sockfd = -1; // khoi tao socket
-	struct sockaddr_in server_addr;	  // chua dia chi cua server
-	char recv_buffer[1024];   // send data client <---> server
+	int sockfd = -1;
+	struct sockaddr_in server_addr;
+	char recv_buffer[1024];
 	time_t ticks;
 
 	memset(recv_buffer, 0, sizeof(recv_buffer));
 	memset(&server_addr, 0, sizeof(server_addr));
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	// AF_INET: truyen du lieu ra ben ngoai
-	// SOCK_STREAM: truyen lien tuc khong ngat quang
-	// protocol: mac dinh
 
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // inet_addr: convert chuoi string ip server theo kieu big-endian
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server_addr.sin_port = htons(5000);
 
 	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
 	{
-		read(sockfd, recv_buffer, sizeof(recv_buffer)-1);
+		read(sockfd, recv_buffer, sizeof(recv_buffer) - 1);
 		printf("\n%s\n", recv_buffer);
 	}
 	close(sockfd);
--- socket-sample/server.c
+++ socket-sample/server.c
@@ -9,37 +9,33 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-	int listenfd = -1;	// khoi tao listen cho socket
-	int connfd = -1;	// khoi tao connect cho socket da ket noi
-	struct sockaddr_in server_addr; // chua dia chi cua server
-	char send_buffer[1024];  // send data client <---> server
+	int listenfd = -1;
+	int connfd = -1;
+	struct sockaddr_in server_addr;
+	char send_buffer[1024];
 	time_t ticks;
 
-	memset (send_buffer, 0, sizeof(send_buffer));
-	memset (&server_addr, 0, sizeof(server_addr));
+	memset(send_buffer, 0, sizeof(send_buffer));
+	memset(&server_addr, 0, sizeof(server_addr));
 
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	// AF_INET: truyen du lieu ra ben ngoai
-	// SOCK_STREAM: truyen lien tuc khong ngat quang
-	// protocol: mac dinh
-
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // // inet_addr: convert chuoi string ip server theo kieu big-endian
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server_addr.sin_port = htons(5000);
-	
-	bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)); // dinh kem dai chi vao socket
-	listen(listenfd, 10); // max 10 client connect den
+
+	bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	listen(listenfd, 10);
 
 	while (1)
 	{
-		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); // chap nhan ket noi tu client
-		ticks = time(NULL);  // lay thoi gian hien tai 
-		sprintf(send_buffer, "server reply %s",ctime(&ticks));  // dinh dang phan hoi la gian hien tai cua server
-		write(connfd, send_buffer, strlen(send_buffer));  // send-buffer den client bang socket da ket noi
-		close(connfd); // dong socket da ket noi sau khi gui phan hoi
+		connfd = accept(listenfd, (struct sockaddr *)NULL, NULL);
+		ticks = time(NULL);
+		sprintf(send_buffer, "server reply %s", ctime(&ticks));
+		write(connfd, send_buffer, strlen(send_buffer));
+		close(connfd);
 	}
-	
+
 	return 0;
 }
